d03f90e6d96c9dabc89e6a94851a531c64e19dea
max9147 <73356484+max9147@users.noreply.github.com>
2024-09-25 13:47:18 +0400

    Added score animation on match

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +10,8 @@ public class ScoringSystem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _comboText;
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private TextMeshProUGUI _turnsText;
+    [SerializeField] private RectTransform _addedScorePrefab;
+    [SerializeField] private RectTransform _statsContainer;
 
     private int _currentCombo;
     private int _currentScore;
@@ -25,6 +29,8 @@ public class ScoringSystem : MonoBehaviour
 
         if (_isCorrect)
         {
+            StartCoroutine(ShowingAddedScore(_currentCombo));
+
             _currentScore += _currentCombo;
             _scoreText.text = _currentScore.ToString();
             _currentCombo++;
@@ -46,5 +52,21 @@ public class ScoringSystem : MonoBehaviour
         _comboText.text = $"{_currentCombo}x";
         _scoreText.text = _currentScore.ToString();
         _turnsText.text = _currentTurns.ToString();
+    }
+
+    private IEnumerator ShowingAddedScore(int _setScore)
+    {
+        RectTransform _currentAddedScore = Instantiate(_addedScorePrefab, _statsContainer);
+        _currentAddedScore.GetComponent<TextMeshProUGUI>().text = $"+{_setScore}";
+        _currentAddedScore.anchoredPosition = _scoreText.GetComponent<RectTransform>().anchoredPosition + new Vector2(100f, 0f);
+        _currentAddedScore.DOAnchorPosY(_currentAddedScore.anchoredPosition.y + 100f, 0.5f);
+
+        yield return new WaitForSeconds(0.25f);
+
+        _currentAddedScore.GetComponent<TextMeshProUGUI>().DOColor(new Color(0f, 1f, 0f, 0f), 0.25f);
+
+        yield return new WaitForSeconds(0.25f);
+
+        Destroy(_currentAddedScore.gameObject);
     }
 }
